b2a9117193d2a0ceb96e4e14c95061ca6ea520ad
Jinwon-Dev <yjw9424@naver.com>
2023-05-17 00:46:49 +0900

    feat: Publisher로부터 주기적으로 센싱값을 받아 외부 API로 송신하는 기능 추가

--- src/main/java/io/wisoft/mqtt/application/MqttSubscriberService.java
+++ src/main/java/io/wisoft/mqtt/application/MqttSubscriberService.java
@@ -1,15 +1,19 @@
 package io.wisoft.mqtt.application;
 
 import org.eclipse.paho.client.mqttv3.*;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class MqttSubscriberService implements MqttCallback {
 
     private MqttClient mqttClient;
     private MqttConnectOptions mqttOptions;
 
-    public MqttSubscriberService init(String server, String clientId) throws MqttException {
+    @Autowired
+    private WebClientService webClientService;
+
+    public MqttSubscriberService init(final String server, final String clientId) throws MqttException {
 
         mqttOptions = new MqttConnectOptions();
         mqttOptions.setCleanSession(true);
@@ -23,20 +27,22 @@ public class MqttSubscriberService implements MqttCallback {
 
     // 커넥션이 종료되면 호출 - 통신 오류로 연결이 끊어지는 경우 호출
     @Override
-    public void connectionLost(Throwable cause) {
+    public void connectionLost(final Throwable cause) {
         System.out.println("연결이 중단되었습니다.");
+        System.out.println(cause);
     }
 
     // 메시지가 도착하면 호출
     @Override
-    public void messageArrived(String topic, MqttMessage message) throws Exception {
+    public void messageArrived(final String topic, final MqttMessage message) throws Exception {
         System.out.println("메시지도착");
         System.out.println(message);
         System.out.println("topic = " + topic + ", id = " + message.getId() + ", payload = " + new String(message.getPayload()));
+        webClientService.post(message);
     }
 
     // 구독 신청
-    public boolean subscribe(String topic) throws MqttException {
+    public boolean subscribe(final String topic) throws MqttException {
 
         if (topic != null) {
             mqttClient.subscribe(topic, 0);
@@ -47,7 +53,6 @@ public class MqttSubscriberService implements MqttCallback {
 
     // 메시지의 배달이 완료되면 호출
     @Override
-    public void deliveryComplete(IMqttDeliveryToken token) {
-
+    public void deliveryComplete(final IMqttDeliveryToken token) {
     }
 }
