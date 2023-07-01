9f0ae25c3d1ca4e07185640b6fec8c86444ab1da
Jinwon-Dev <yjw9424@naver.com>
2023-07-01 19:59:44 +0900

    feat: 인증 처리 과정 추가에 따른 로직 & 형태 수정

--- src/main/java/io/wisoft/mqtt/application/WebClientService.java
+++ src/main/java/io/wisoft/mqtt/application/WebClientService.java
@@ -1,13 +1,17 @@
 package io.wisoft.mqtt.application;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.wisoft.mqtt.config.MqttConfig;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.StringTokenizer;
 
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.MediaType.*;
@@ -17,9 +21,52 @@ public class WebClientService {
 
     MqttConfig mqttConfig = new MqttConfig();
 
-    public void requestToApiServer(final MqttMessage message) {
+    public static void post(final MqttMessage message) throws JsonProcessingException {
+        final WebClientService webClientService = new WebClientService();
+        webClientService.splitJsonData(message);
+    }
 
-        Map<String, Object> bodyMap = extractdata(message);
+    public void splitJsonData(final MqttMessage message) throws JsonProcessingException {
+
+        String json = new String(message.getPayload());
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        List<Map<String, Object>> dataList = objectMapper.readValue(json, new TypeReference<>() {});
+        List<Map<String, Object>> requestDataList = new ArrayList<>();
+
+        for (Map<String, Object> jsonData : dataList) {
+            Map<String, Object> bodyMap = extractData(jsonData);
+            requestDataList.add(bodyMap);
+        }
+
+        String accessToken = extractToken(dataList);
+        requestToApiServer(requestDataList, accessToken);
+    }
+
+    private Map<String, Object> extractData(Map<String, Object> jsonData) {
+
+        String value = jsonData.get("value").toString();
+        String measurement = jsonData.get("measurement").toString();
+
+        final Map<String, Object> bodyMap = new HashMap<>();
+        bodyMap.put("value", value);
+        bodyMap.put("measurement", measurement);
+
+        return bodyMap;
+    }
+
+    private String extractToken(List<Map<String, Object>> dataList) {
+
+        if (!dataList.isEmpty()) {
+            Map<String, Object> firstData = dataList.get(0);
+            if (firstData.containsKey("accessToken")) {
+                return firstData.get("accessToken").toString();
+            }
+        }
+        return null;
+    }
+
+    public void requestToApiServer(final List<Map<String, Object>> bodyMap, final String accessToken) {
 
         final WebClient webClient = WebClient
                 .builder()
@@ -27,40 +74,16 @@ public class WebClientService {
                 .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
                 .build();
 
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("dataList", bodyMap);
+
         final Map<String, Object> response = (Map<String, Object>) webClient
                 .post()
                 .uri(mqttConfig.apiUrl)
-                .bodyValue(bodyMap)
+                .header("accessToken", accessToken)
+                .bodyValue(requestBody)
                 .retrieve()
                 .bodyToMono(Map.class)
                 .block();
-    }
-
-    public static void post(final MqttMessage message) {
-        final WebClientService webClientService = new WebClientService();
-        webClientService.requestToApiServer(message);
-    }
-
-    private Map<String, Object> extractdata(final MqttMessage message) {
-
-        final Map<String, Object> bodyMap = new HashMap<>();
-
-        final String str = new String(message.getPayload());
-        final StringTokenizer st = new StringTokenizer(str, "=,");
-
-        st.nextToken();
-        final Long id = Long.valueOf(st.nextToken());
-
-        st.nextToken();
-        final Double number = Double.valueOf(st.nextToken());
-
-        st.nextToken();
-        final String measurement = st.nextToken();
-
-        bodyMap.put("memberId", id);
-        bodyMap.put("value", number);
-        bodyMap.put("measurement", measurement);
-
-        return bodyMap;
     }
 }
